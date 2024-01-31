cae1db14d73ecbe11915ac409d21cd7c9a8dcaa6
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-01-31 16:48:00 -0500

    style: add chakra provider to app with blues from icon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const colors = {
+  brand: {
+    900: "#042433",
+    800: "#76A1BC",
+    700: "#D6E7E4",
+  },
+}
+
+const theme = extendTheme({ colors })
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>
-);
+  </ChakraProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
