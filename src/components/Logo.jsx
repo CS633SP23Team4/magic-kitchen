f2a1fa60d6808c8d3cba734cd934501735e388fd
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-01 20:07:47 -0500

    lint: add proptypes for prop validation

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,12 @@
 import React from "react"
 import { Box, Image } from "@chakra-ui/react"
+import PropTypes from "prop-types"
 import logo from "../logo.png"
 
+Logo.propTypes = {
+  size: PropTypes.string,
+  radius: PropTypes.string,
+}
 export default function Logo(props) {
   return (
     <Box>
