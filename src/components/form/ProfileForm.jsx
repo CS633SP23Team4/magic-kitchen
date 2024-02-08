51fa837366e841ca908717490234b0c1a5d437c8
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-08 14:21:11 -0500

    feat: init profile form

---
+++ src/components/form/ProfileForm.jsx
@@ -0,0 +1,95 @@
+import {useEffect, useState} from "react"
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    Avatar,
+    InputGroup,
+    Textarea,
+    Flex, AvatarBadge, Button,
+} from "@chakra-ui/react"
+import {NotAllowedIcon, EditIcon} from "@chakra-ui/icons";
+import Select, {components, OptionProps} from "react-select"
+import {DietOption, DietOptions} from "../../data/diet.ts"
+
+const Option = (props: OptionProps<DietOption>) => {
+    return (
+        <div
+            style={{
+                padding: "2px",
+                display: "flex",
+                alignItems: "center",
+                border: "1px solid",
+                justifyContent: "space-between"
+            }}
+        >
+            <NotAllowedIcon color='red.500'/>
+            <components.Option {...props} />
+        </div>
+    );
+};
+
+export default function ProfileForm() {
+    const [nameFirst, setNameFirst] = useState("")
+    const [nameLast, setNameLast] = useState("")
+    const [bio, setBio] = useState("")
+    const [storeImage, setStoreImage] = useState([])
+    const saveImage = (img) => {
+        setStoreImage(img)
+    }
+    return (
+        <>
+            <FormControl>
+                <FormLabel fontSize={24}>Avatar</FormLabel>
+                <Flex justify="center">
+                    <Avatar size="2xl" name="Christian Nwamba" src="https://bit.ly/code-beast">{" "}
+                        <AvatarBadge as={Button} boxSize='3em'><EditIcon/></AvatarBadge>
+                    </Avatar>
+                </Flex>
+            </FormControl>
+            <FormControl pt={4}>
+                <FormLabel fontSize={24}>Name</FormLabel>
+                <InputGroup pt={2}>
+                    <Input
+                        bg="gray.50"
+                        type="text"
+                        placeholder="First Name"
+                        value={nameFirst}
+                        onChange={(e) => setNameFirst(e.target.value)}
+                    />
+                    <Input
+                        ml={2}
+                        bg="gray.50"
+                        type="text"
+                        placeholder="Last Name"
+                        value={nameLast}
+                        onChange={(e) => setNameLast(e.target.value)}
+                    />
+                </InputGroup>
+            </FormControl>
+            <FormControl pt={4}>
+                <FormLabel fontSize={24}>Dietary Restrictions</FormLabel>
+                <Select
+                    closeMenuOnSelect={false}
+                    components={{Option}}
+                    styles={{
+                        option: (base) => ({
+                            ...base
+                        })
+                    }}
+                    isMulti
+                    options={DietOptions}
+                />
+            </FormControl>
+            <FormControl pt={4}>
+                <FormLabel fontSize={24}>Bio</FormLabel>
+                <Textarea
+                    bg="gray.50"
+                    placeholder="(optional) Enter a few lines about yourself"
+                    value={bio}
+                    onChange={(e) => setBio(e.target.value)}
+                />
+            </FormControl>
+        </>
+    )
+}
---
+++ src/data/diet.ts
@@ -0,0 +1,30 @@
+export interface DietOption {
+  readonly value: string
+  readonly label: string
+}
+
+export const DietOptions: readonly DietOption[] = [
+  { value: "egg", label: "Eggs" },
+  { value: "cowmilk", label: "Cow Milk" },
+  { value: "soy", label: "Soy" },
+  { value: "treenut", label: "Tree Nuts" },
+  { value: "peanut", label: "Peanuts" },
+  { value: "fish", label: "Fish" },
+  { value: "shellfish", label: "Shellfish" },
+  { value: "sesame", label: "Sesame" },
+  { value: "wheat", label: "Wheat" },
+  { value: "triticale", label: "Triticale" },
+  { value: "celery", label: "Celery" },
+  { value: "carrot", label: "Carrots" },
+  { value: "avacado", label: "Avacado" },
+  { value: "bellpepper", label: "Bell Pepper" },
+  { value: "potato", label: "Potatoes" },
+  { value: "pumpkin", label: "Pumpkins" },
+  { value: "mushroom", label: "Mushrooms" },
+  { value: "onion", label: "Onions" },
+  { value: "mustard", label: "Mustard" },
+  { value: "pork", label: "Pork" },
+  { value: "beef", label: "Beef" },
+  { value: "chicken", label: "Chicken" },
+  { value: "dairy", label: "Dairy" },
+]
