d1b2639c7dd0c65e655a49eea27324976c661c8b
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-07 23:07:45 -0500

    refactor: remove shared components

--- src/components/form/RecipeForm.jsx
+++ src/components/form/RecipeForm.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 import {
   FormControl,
   FormLabel,
-  FormErrorMessage,
   FormHelperText,
   Input,
   Button,
@@ -16,6 +15,7 @@ import {
 } from "@chakra-ui/react"
 import { DeleteIcon } from "@chakra-ui/icons"
 import PropTypes from "prop-types"
+import { PrimaryButton, TertiaryButton, DeleteButton, SecondaryButton } from "./CustomButton"
 
 IngredientGroup.propTypes = {
   id: PropTypes.string,
@@ -44,9 +44,7 @@ function IngredientGroup(props) {
         value={amount}
         onChange={(e) => setAmount(e.target.value)}
       />
-      <Button border="1px" borderColor="gray.200" onClick={props.deleteFunction}>
-        <DeleteIcon />
-      </Button>
+      <DeleteButton deleteFunction={props.deleteFunction} />
     </InputGroup>
   )
 }
@@ -58,9 +56,7 @@ function StepsGroup(props) {
         Step {parseInt(props.id) + 1}
       </Text>
       <Textarea bg="gray.50" height={48} placeholder="Add step instructions"></Textarea>
-      <Button border="1px" borderColor="gray.200" size="xs" onClick={props.deleteFunction}>
-        <DeleteIcon boxSize={3} />
-      </Button>
+      <DeleteButton deleteFunction={props.deleteFunction} />
     </Flex>
   )
 }
@@ -92,92 +88,67 @@ export default function CustomRecipe() {
     setSteps(steps.concat([{ id: `${steps.length}`, instructions: "" }]))
   }
 
+  const fewerSteps = (i) => {
+    setSteps((prevSteps) => {
+      return prevSteps.filter((obj) => obj.id !== i)
+    })
+  }
+
   return (
-    <Box px={24} bg="#f0f9fa" align="center" justifyContent="center">
-      <Box p={2}>
-        <Box textAlign="center">
-          <Heading>Add a New Recipe</Heading>
-        </Box>
-        <Box my={4} textAlign="left">
-          <form>
-            <FormControl isRequired>
-              <FormLabel fontSize={24}>Name</FormLabel>
-              <Input
-                bg="gray.50"
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <FormHelperText>Enter the name of this recipe.</FormHelperText>
-            </FormControl>
-            <FormControl my={6} isRequired>
-              <Flex py={2} align={"center"}>
-                <FormLabel fontSize={24} m={0}>
-                  Ingredients
-                </FormLabel>
-                <Button border="1px" borderColor="gray.200" ml={6} onClick={moreIngredients}>
-                  Add Ingredient
-                </Button>
-              </Flex>
-              {ingredients.map((ingredient) => (
-                <IngredientGroup
-                  id={ingredient.id}
-                  key={ingredient.id}
-                  deleteFunction={() => fewerIngredients(ingredient.id)}
-                />
-              ))}
-            </FormControl>
-            <FormControl my={6} isRequired>
-              <Flex py={2} align={"center"}>
-                <FormLabel fontSize={24} m={0}>
-                  Steps
-                </FormLabel>
-                <Button border="1px" borderColor="gray.200" ml={6} onClick={moreSteps}>
-                  Add Step
-                </Button>
-              </Flex>
-              <Stack>
-                {steps.map((step) => (
-                  <StepsGroup id={step.id} key={step.id} />
-                ))}
-              </Stack>
-            </FormControl>
-            <FormControl my={6} isRequired>
-              <FormLabel fontSize={24}>Est Time</FormLabel>
-              <Input
-                bg="gray.50"
-                type="text"
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
-              />
-              <FormHelperText>Estimated cooking time</FormHelperText>
-            </FormControl>
-            <FormControl my={6}>
-              <FormLabel fontSize={24}>Ktichenware Needed</FormLabel>
-              <Input
-                bg="gray.50"
-                type="text"
-                value={kitchenware}
-                onChange={(e) => setKitchenware(e.target.value)}
-              />
-              <FormHelperText>What cookware will you need?</FormHelperText>
-            </FormControl>
-            <FormControl my={6}>
-              <FormLabel fontSize={24}>Tips</FormLabel>
-              <Input
-                bg="gray.50"
-                type="text"
-                value={tips}
-                onChange={(e) => setTips(e.target.value)}
-              />
-              <FormHelperText>Any common mistakes or tips</FormHelperText>
-            </FormControl>
-            <Button bg="brand.900" color="gray.50" width="full" mt={4} type="submit">
-              Submit
-            </Button>
-          </form>
-        </Box>
-      </Box>
-    </Box>
+    <div>
+      <FormControl isRequired>
+        <FormLabel fontSize={24}>Name</FormLabel>
+        <Input bg="gray.50" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <FormHelperText>Enter the name of this recipe.</FormHelperText>
+      </FormControl>
+      <FormControl my={6} isRequired>
+        <Flex py={2} align={"center"}>
+          <FormLabel fontSize={24} m={0}>
+            Ingredients
+          </FormLabel>
+          <SecondaryButton text="Add Ingredient" clickFunction={moreIngredients} />
+        </Flex>
+        {ingredients.map((ingredient) => (
+          <IngredientGroup
+            id={ingredient.id}
+            key={ingredient.id}
+            deleteFunction={() => fewerIngredients(ingredient.id)}
+          />
+        ))}
+      </FormControl>
+      <FormControl my={6} isRequired>
+        <Flex py={2} align={"center"}>
+          <FormLabel fontSize={24} m={0}>
+            Steps
+          </FormLabel>
+          <SecondaryButton text="Add Step" clickFunction={moreSteps} />
+        </Flex>
+        <Stack>
+          {steps.map((step) => (
+            <StepsGroup id={step.id} key={step.id} deleteFunction={() => fewerSteps(step.id)} />
+          ))}
+        </Stack>
+      </FormControl>
+      <FormControl my={6} isRequired>
+        <FormLabel fontSize={24}>Est Time</FormLabel>
+        <Input bg="gray.50" type="text" value={time} onChange={(e) => setTime(e.target.value)} />
+        <FormHelperText>Estimated cooking time</FormHelperText>
+      </FormControl>
+      <FormControl my={6}>
+        <FormLabel fontSize={24}>Ktichenware Needed</FormLabel>
+        <Input
+          bg="gray.50"
+          type="text"
+          value={kitchenware}
+          onChange={(e) => setKitchenware(e.target.value)}
+        />
+        <FormHelperText>What cookware will you need?</FormHelperText>
+      </FormControl>
+      <FormControl my={6}>
+        <FormLabel fontSize={24}>Tips</FormLabel>
+        <Input bg="gray.50" type="text" value={tips} onChange={(e) => setTips(e.target.value)} />
+        <FormHelperText>Any common mistakes or tips</FormHelperText>
+      </FormControl>
+    </div>
   )
 }
