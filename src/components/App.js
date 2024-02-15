328fc6f77b4957e029a794eeee1a32feb84738c6
tony <qtx2023@bu.edu>
2024-02-15 18:38:43 -0500

    Local version created.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar"
+import BlogList from "./BlogList"
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
         <Navbar />
       </header>
+      < BlogList />
     </div>
   )
 }
