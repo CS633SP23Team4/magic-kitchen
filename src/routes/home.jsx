6dc45e0762ffe8d04d436e02bca29cf95dae0906
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-16 21:25:18 -0500

    feat: add logo and headers

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,9 +1,21 @@
 import App from "../components/App"
+import { Logo } from "../components/Logo"
+import Layout from "../components/Layout"
+import { Flex, Heading, Stack, Text } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <>
-      <App />
-    </>
+    <Layout>
+      <Flex flexDir="column" align="center">
+        <Stack py={6} direction="row" alignItems="center" justifyContent="center">
+          <Logo />
+          <Heading size="4xl" fontFamily="Aladin">
+            Magic Kitchen
+          </Heading>
+        </Stack>
+
+        <Heading size="sm">Just enter your ingredients and let the magic happen.</Heading>
+      </Flex>
+    </Layout>
   )
 }
