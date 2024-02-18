82387b5b7b4418c2a1c35d9c4afd41bde49d06f2
Tony <qtx2023@bu.edu>
2024-02-18 10:26:36 -0500

    Data transferred from blogs, blog page will be discarded.

--- src/data/tips.ts
+++ src/data/tips.ts
@@ -64,4 +64,33 @@ export const TipsData: readonly Tips[] = [
         link: "https://iowagirleats.com/40-favorite-kitchen-tips-and-tricks/",
         image_url: "https://iowagirleats.com/wp-content/uploads/2014/10/SlicedBasil.jpg",
     },
+    {
+        id: "6",
+        title: "Here's How To Eat Healthy For A Week With Just $50",
+        content:
+          "Join writers Wendy and Jess in a challenge to eat healthy for an entire week for under $50.",
+        alt: "A gallery of finished recipes (apples, salad, etc)",
+        link: "https://tasty.co/article/jessicamjones/eat-healthy-all-week-for-50-bucks",
+        image_url:
+          "https://img.buzzfeed.com/buzzfeed-static/static/2016-07/7/16/asset/buzzfeed-prod-fastlane03/sub-buzz-16736-1467924099-1.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto",
+    },
+    {
+        id: "7",
+        title: "School Packed Lunch Ideas",
+        content:
+          "Thinking of healthy and exciting packed lunches can be a daily challenge - especially if you can't envisage your child happily trotting to school with a box of sprouting mung beans. Here are some simple, fast, and scrumptious ideas to bring new life to the dreaded lunchbox.",
+        alt: "Meat wraps in a tray with strawberries",
+        link: "https://www.bbcgoodfood.com/howto/guide/school-packed-lunch-inspiration",
+        image_url:
+          "https://images.immediate.co.uk/production/volatile/sites/30/2022/06/Hero-image-0943d18.jpg?quality=90&webp=true&resize=600,545",
+    },
+    {
+        id: "8",
+        title: "50 Date Night Dinner Ideas",
+        content:
+          "Do you have a romantic date night dinner coming up that you want to make extra special? It all starts with the perfect dinner main course — something that is delicious, fancy, and comforting.",
+        alt: "Meat wraps in a tray with strawberries",
+        link: "https://www.aheadofthyme.com/50-date-night-dinner-ideas/",
+        image_url: "https://www.aheadofthyme.com/wp-content/uploads/2021/08/50-date-night-dinner-ideas.jpg",
+    },
 ]
--- src/routes/tips.jsx
+++ src/routes/tips.jsx
@@ -3,11 +3,13 @@ import {Box, Heading} from "@chakra-ui/react"
 import {TipsData} from "../data/tips.ts"
 import { BlogCard } from "../components/Card"
 import Layout from "../components/Layout";
+import FaqSection from "../components/FaqSection"
+import { FaqOptions } from "../data/faq.ts"
 function TipsList() {
     return (
         <Layout>
-            <Heading margin="5vh">Tips</Heading>
             <Box my={8} textAlign="center">
+              <Heading margin="5vh">Tips</Heading>
                 <Box display="grid" gridGap="4" gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))">
                     {TipsData.map((tip) => (
                         <BlogCard // Use BlogCard component
@@ -20,6 +22,8 @@ function TipsList() {
                         />
                     ))}
                 </Box>
+              <Heading margin="5vh">Frequently Asked Questions</Heading>
+              <FaqSection items={FaqOptions} />
             </Box>
         </Layout>
     );
