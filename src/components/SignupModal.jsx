93c1b725acadef7ba01543cce103202bd764dc73
Adriel Domingo <adrieldomingo@gmail.com>
2024-02-18 11:50:20 -0500

    feat-Google_signin

--- src/components/SignupModal.jsx
+++ src/components/SignupModal.jsx
@@ -6,13 +6,48 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  ButtonGroup,
   Text,
   Button,
   useDisclosure,
+  Input,
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
 } from "@chakra-ui/react"
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import {
+  auth,
+  logInWithEmailAndPassword,
+  signInWithGoogle,
+} from '../firebaseInit';
 
 export function SignupModal() {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  //const auth = getAuth(app);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  const [user, setUser] = useState("")
+  const SignIn = async () => {
+    const user = await signInWithGoogle()
+    setUser(user.email)
+    console.log(user)
+  } 
+
+  // useEffect(() => {
+  //   if (loading) {
+  //     // maybe trigger a loading screen
+  //     return;
+  //   }
+  //   if (user) navigate('/');
+  // }, [user, loading, navigate]);
+  
   return (
     <>
       <Button onClick={onOpen}>Sign in / Sign Up</Button>
@@ -23,14 +58,42 @@ export function SignupModal() {
           <ModalHeader>Sign in / Sign Up</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text>INSERT FIREAUTH HERE</Text>
+          <FormControl isRequired>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="email"
+              type="email"
+              autoComplete="email"
+            />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                placeholder="Password"
+                autoComplete="current-password"
+              />
+            </FormControl>
+            <Button w="full" my="2" >
+              Login
+            </Button>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
+            <ButtonGroup variant='solid' spacing='122' alignItems="center">
+                <Button colorScheme="blue" onClick={SignIn}>
+                  Login with Google
+                </Button>
+                <Button colorScheme="blue" variant="link" onClick={null}>
+                  Register
+                </Button>
+              </ButtonGroup>
+            {user}
+
           </ModalFooter>
         </ModalContent>
       </Modal>
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,5 +1,23 @@
 import { initializeApp } from "firebase/app"
 
+import { 
+  GoogleAuthProvider, 
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
@@ -10,4 +28,81 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+export const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const credential = GoogleAuthProvider.credentialFromResult(res);
+    const user = res.user;
+    return (user)
+    // const q = query(collection(db, 'users'), where('uid', '==', user.uid));
+    // const docs = await getDocs(q);
+    // if (docs.docs.length === 0) {
+    //   await addDoc(collection(db, 'users'), {
+    //     uid: user.uid,
+    //     name: user.displayName,
+    //     authProvider: 'google',
+    //     email: user.email,
+    //   });
+    // }
+
+  } catch (err) {
+    console.error(err);
+    throw new Error(err)
+  }
+};
+
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async email => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert('Password reset link sent!');
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+// export {
+//   app,
+//   auth,
+//   db,
+//   signInWithGoogle,
+//   logInWithEmailAndPassword,
+//   registerWithEmailAndPassword,
+//   sendPasswordReset,
+//   logout,
+// };
