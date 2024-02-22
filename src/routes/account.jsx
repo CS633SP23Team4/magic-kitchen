1c0af7391533ff8158f734fe64bbb1b648455285
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-22 16:44:12 -0500

    feat: change view based on side buttons

--- src/routes/account.jsx
+++ src/routes/account.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react"
+import PropTypes from "prop-types"
+import { Box, Button, Flex, Grid, GridItem, Stack, Text } from "@chakra-ui/react"
 import Layout from "../components/Layout"
-import { Box, Flex, Grid, GridItem, Heading, Stack, Text } from "@chakra-ui/react"
 import ProfileForm from "../components/form/ProfileForm"
 import { FormWrapper } from "../components/form/FormWrapper"
 
+UpdateView.propTypes = {
+  value: PropTypes.string,
+}
+
+function UpdateView(props) {
+  switch (props.value) {
+    case "password":
+      return (
+        <>
+          <Text>FIREAUTH STUFF</Text>
+        </>
+      )
+    case "preferences":
+      return (
+        <FormWrapper>
+          <Text>IDK WHAT GOES HERE</Text>
+        </FormWrapper>
+      )
+    default:
+      return (
+        <FormWrapper header="Personal Information">
+          <ProfileForm />
+        </FormWrapper>
+      )
+  }
+}
+
 export default function Account() {
+  const [currentView, setCurrentView] = useState("personal")
   return (
     <Layout>
       <Grid pt="12" m={0} templateColumns="repeat(4, 1fr)" gap={6}>
@@ -11,24 +41,26 @@ export default function Account() {
           <Stack p={8} borderWidth="1px" borderRadius="lg" shadow="md">
             <Flex alignItems="center" my={2}>
               <Box w="12px" h="12px" bg="coral" m={2} />
-              <Text>Personal Info</Text>
+              <Button variant="link" onClick={() => setCurrentView("personal")}>
+                Personal Info
+              </Button>
             </Flex>
             <Flex alignItems="center" my={2}>
               <Box w="12px" h="12px" bg="darkcyan" m={2} />
-              <Text>Change Password</Text>
+              <Button variant="link" onClick={() => setCurrentView("password")}>
+                Change Password
+              </Button>
             </Flex>
             <Flex alignItems="center" my={2}>
               <Box w="12px" h="12px" bg="deeppink" m={2} />
-              <Text>Preferences</Text>
+              <Button variant="link" onClick={() => setCurrentView("preferences")}>
+                Preferences
+              </Button>
             </Flex>
           </Stack>
         </GridItem>
-
-        <GridItem colSpan={3}>
-          <FormWrapper header="Personal Information">
-            <ProfileForm />
-          </FormWrapper>
-        </GridItem>
+        {/*BELOW GRID ITEM CHANGE VIEW BASED ON ABOVE CLICKED ELEMENT*/}
+        <GridItem colSpan={3}>{<UpdateView value={currentView} />}</GridItem>
       </Grid>
     </Layout>
   )
