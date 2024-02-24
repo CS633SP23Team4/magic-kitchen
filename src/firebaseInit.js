091c9031596ed9abc0984b6559ad9ed8b77e6a3d
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-24 16:06:03 -0500

    test: rename pi key

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app"
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
 }
-
+console.log(firebaseConfig)
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
