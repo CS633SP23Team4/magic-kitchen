a4bccd8d37bc211752e2b9c1122b6f3eb84de221
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-24 18:41:32 -0500

    feat: firestore init

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,4 +1,14 @@
 import { initializeApp } from "firebase/app"
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
@@ -8,6 +18,35 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
 }
-
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+
+export async function getUserRecipes(user) {
+  const recipeQuery = query(collection(db, "userRecipes"), where("UserId", "==", user))
+  const querySnapshot = await getDocs(recipeQuery)
+  const recipeArray = []
+  querySnapshot.forEach((doc) => {
+    const document = { ...doc.data(), id: doc.id }
+    recipeArray.push(document)
+  })
+  return recipeArray
+}
+
+export async function pushUserRecipe(user, data) {
+  try {
+    await setDoc(doc(db, "userRecipes", data.id), { data }, { merge: true })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function getUserFavorites(user) {
+  const querySnapshot = await getDocs(collection(db, "users", user, "LikedRecipes"))
+  const recipeArray = []
+  querySnapshot.forEach((doc) => {
+    const document = { ...doc.data(), id: doc.id }
+    recipeArray.push(document)
+  })
+  return recipeArray
+}
