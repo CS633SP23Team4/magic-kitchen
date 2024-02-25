d1906024ed44cce894677d68c5dc40c2e14ade21
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-25 13:20:49 -0500

    feat: limit api recipes to 10

--- src/spoonacular.js
+++ src/spoonacular.js
@@ -2,7 +2,7 @@ export async function getRecipeFromIngredients(selectedIngredients) {
   const ingredientIds = selectedIngredients.map((ingredient) => ingredient.value)
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&number=20&apiKey=075d758830d041ea9b8ae7f3e3b3c8e3`
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&apiKey=075d758830d041ea9b8ae7f3e3b3c8e3`
     )
     return await response.json()
   } catch (error) {
