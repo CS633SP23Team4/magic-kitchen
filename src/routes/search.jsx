c8a3f587059bc0f0f34507880fe644c3d350d8b2
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-25 13:20:39 -0500

    fix: rehydrate data when recipes changes

--- src/routes/search.jsx
+++ src/routes/search.jsx
@@ -35,7 +35,7 @@ export default function Search() {
       )
     }
     setDisplayedRecipes(filteredRecipes)
-  }, [cookingTimeOption])
+  }, [recipes, cookingTimeOption])
 
   return (
     <Layout>
