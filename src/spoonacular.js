b6088e4122b071c4c9b963ddbf0d8ee8bd972016
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-25 13:45:31 -0500

    chore: update api key

--- src/spoonacular.js
+++ src/spoonacular.js
@@ -2,7 +2,7 @@ export async function getRecipeFromIngredients(selectedIngredients) {
   const ingredientIds = selectedIngredients.map((ingredient) => ingredient.value)
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&apiKey=075d758830d041ea9b8ae7f3e3b3c8e3`
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&apiKey=597bacfd71824f7586aad89a023bddb8`
     )
     return await response.json()
   } catch (error) {
@@ -13,7 +13,7 @@ export async function getRecipeFromIngredients(selectedIngredients) {
 export async function getBulkRecipeInformation(ids) {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&apiKey=075d758830d041ea9b8ae7f3e3b3c8e3`
+      `https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&apiKey=597bacfd71824f7586aad89a023bddb8`
     )
     return await response.json()
   } catch (error) {
