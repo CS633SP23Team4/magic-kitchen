a817035f669404f36a3e555d17fd5eb68298a639
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-26 14:29:45 -0500

    refactor: pass user to profile form

--- src/routes/account.jsx
+++ src/routes/account.jsx
@@ -1,23 +1,42 @@
 import { useState } from "react"
+import { useLocation } from "react-router-dom"
 import PropTypes from "prop-types"
 import { Box, Button, Flex, Grid, GridItem, Stack, Text } from "@chakra-ui/react"
 import { resetPassword } from "../firebaseInit"
 import Layout from "../components/Layout"
 import ProfileForm from "../components/form/ProfileForm"
 import { FormWrapper } from "../components/form/FormWrapper"
-import CustomRecipe from "../components/form/RecipeForm"
+import { CreateNewRecipeForm } from "../components/form/RecipeWrapper"
 
 UpdateView.propTypes = {
   value: PropTypes.string,
+  user: PropTypes.string,
 }
 
 function UpdateView(props) {
+  const [message, setMessage] = useState("")
+
+  const resetMyPass = async (user) => {
+    const response = await resetPassword(user)
+    setMessage(response)
+  }
   switch (props.value) {
     case "password":
       return (
         <>
-          <Text>Reset Password Email</Text>
-          <Button onClick={resetPassword}>Reset</Button>
+          <Flex
+            bg="white"
+            flexDir="column"
+            align="center"
+            justify="center"
+            boxShadow="md"
+            p={4}
+            m={4}
+          >
+            <Text color={"green"}>{message}</Text>
+            <Text>Send Email to Reset Password</Text>
+            <Button onClick={() => resetMyPass(props.user)}>Reset</Button>
+          </Flex>
         </>
       )
     case "preferences":
@@ -27,11 +46,11 @@ function UpdateView(props) {
         </FormWrapper>
       )
     case "create":
-      return <CustomRecipe />
+      return <CreateNewRecipeForm />
     default:
       return (
         <FormWrapper header="Personal Information">
-          <ProfileForm />
+          <ProfileForm user={props.user} />
         </FormWrapper>
       )
   }
@@ -39,11 +58,14 @@ function UpdateView(props) {
 
 export default function Account() {
   const [currentView, setCurrentView] = useState("personal")
+  const data = useLocation()
+  const user = data.state.user
+
   return (
     <Layout>
       <Grid pt="12" m={0} templateColumns="repeat(4, 1fr)" gap={6}>
         <GridItem colSpan={1}>
-          <Stack p={8} borderWidth="1px" borderRadius="lg" shadow="md">
+          <Stack bg="white" p={8} borderWidth="1px" borderRadius="lg" shadow="md">
             <Flex alignItems="center" my={2}>
               <Box w="12px" h="12px" bg="coral" m={2} />
               <Button variant="link" onClick={() => setCurrentView("personal")}>
@@ -65,7 +87,7 @@ export default function Account() {
           </Stack>
         </GridItem>
         {/*BELOW GRID ITEM CHANGE VIEW BASED ON ABOVE CLICKED ELEMENT*/}
-        <GridItem colSpan={3}>{<UpdateView value={currentView} />}</GridItem>
+        <GridItem colSpan={3}>{<UpdateView value={currentView} user={user} />}</GridItem>
       </Grid>
     </Layout>
   )
