888615aa5e5e42066713b97832c4ef4f7d7456de
tony <qtx2023@bu.edu>
2024-02-26 15:16:44 -0500

    Fixed key redacted.

--- src/spoonacular.js
+++ src/spoonacular.js
@@ -1,8 +1,9 @@
 export async function getRecipeFromIngredients(selectedIngredients) {
   const ingredientIds = selectedIngredients.map((ingredient) => ingredient.value)
   try {
+    const spoon_key = process.env.REACT_APP_SPOON_apiKey
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&apiKey=597bacfd71824f7586aad89a023bddb8`
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&apiKey=${spoon_key}`
     )
     return await response.json()
   } catch (error) {
@@ -12,8 +13,9 @@ export async function getRecipeFromIngredients(selectedIngredients) {
 
 export async function getBulkRecipeInformation(ids) {
   try {
+    const spoon_key = process.env.REACT_APP_SPOON_apiKey
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&apiKey=597bacfd71824f7586aad89a023bddb8`
+      `https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&apiKey=${spoon_key}`
     )
     return await response.json()
   } catch (error) {
