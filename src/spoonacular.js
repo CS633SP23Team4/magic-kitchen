449bdfd3d35a5003214fb86a3b78da72768e952c
tony <qtx2023@bu.edu>
2024-02-26 15:23:11 -0500

    Reformat Spoon key v1

--- src/spoonacular.js
+++ src/spoonacular.js
@@ -3,7 +3,7 @@ export async function getRecipeFromIngredients(selectedIngredients) {
   try {
     const spoon_key = process.env.REACT_APP_SPOON_apiKey
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&apiKey=${spoon_key}`
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientIds.join(",+")}&apiKey=`+spoon_key
     )
     return await response.json()
   } catch (error) {
@@ -15,7 +15,7 @@ export async function getBulkRecipeInformation(ids) {
   try {
     const spoon_key = process.env.REACT_APP_SPOON_apiKey
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&apiKey=${spoon_key}`
+      `https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&apiKey=`+spoon_key
     )
     return await response.json()
   } catch (error) {
